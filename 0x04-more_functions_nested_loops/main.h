dd5c3a121a6ac66dcb2d0cb13c605eae82f5cc24
hammou166 <brozy166@gmail.com>
2023-02-24 08:33:58 -0800

    hey

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -16,3 +16,18 @@ void print_number(int n);
 
 #endif
 
+vi _putchar
+
+#include <unistd.h>
+
+/**
+*_putchar - write the character c to stdout
+*@c:The character to print
+*Return 1 if true, 0 if false
+*On error, -1 return.
+*/
+int_putchar(char c)
+{
+return (write(1, &c, 1));
+}
+
